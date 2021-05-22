378ae6b0548c5cad593ddf65fc4f53306bff8bcf
kiaraSingh <singhneha407@gmail.com>
2021-05-22 12:35:31 +0000

    dummy commit

--- squad4/src/main/java/com/lms/squad4/service/UserService.java
+++ squad4/src/main/java/com/lms/squad4/service/UserService.java
@@ -3,9 +3,11 @@ package com.lms.squad4.service;
 import java.util.List;
 
 import com.lms.squad4.entity.Books;
+import com.lms.squad4.entity.requests.LoginRequest;
 
 public interface UserService {
-
-	String login(Integer userId,String password); 
+	
+	
+	String login(LoginRequest loginRequest); 
 	List<Books> findBooksByUserId(Integer userId);
 }
