77272bf454244ce0c9bcba6031024819e84108cb
kiaraSingh <singhneha407@gmail.com>
2021-05-22 12:31:25 +0000

    added @service

--- squad4/src/main/java/com/lms/squad4/serviceImpl/UserServiceImpl.java
+++ squad4/src/main/java/com/lms/squad4/serviceImpl/UserServiceImpl.java
@@ -2,14 +2,21 @@ package com.lms.squad4.serviceImpl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.lms.squad4.entity.Books;
-import com.lms.squad4.entity.Users;
+import com.lms.squad4.entity.requests.LoginRequest;
 import com.lms.squad4.service.UserService;
 
+@Service
 public class UserServiceImpl implements UserService{
+	
+	@Autowired
+	UserService userService;
 
 	@Override
-	public String login(Integer userId, String password) {
+	public String login(LoginRequest loginRequest) {
 		// TODO Auto-generated method stub
 		return null;
 	}
