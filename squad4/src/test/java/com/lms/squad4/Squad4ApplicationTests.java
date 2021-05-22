c1e5039330c6eb7058d6edde3e2105bef85869a5
Satya <satya.katakamsetti@shutterfly.com>
2021-05-22 15:30:47 +0530

    created project

---
+++ squad4/src/test/java/com/lms/squad4/Squad4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lms.squad4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Squad4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
